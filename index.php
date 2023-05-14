025a45cddb4f93be6cd40f49c7d7eb210b9e911f
Danmar Cornelio Nacario <dcnacario08@gmail.com>
2023-05-15 00:53:53 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Aerilon | The world is yours to explore</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" type="image-x-icon" href="./resources/favicon.ico">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="./css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet">
+    <body>
+        <ul class="topnav">
+            <li class="logo"><img src="./resources/logo.svg" width="10%"></li>
+            <div class="right_group">
+                <li class="right_search"><input type="text" placeholder="Search" class="search_bar"></li>
+                <li class="right"><a href="#">Meet the Crew</a></li>
+            </div>
+        </ul>
+        <span class="content-text">The world is yours to <span class="content-colored">EXPLORE</span></span>
+        <div class="content">
+        <div class="sub-content">      
+        </div>
+        </div>
+    </body>
+</html>
