ba79dfc6ad34e5084f7cc339c4b33a33624f3b12
Danmar Cornelio Nacario <dcnacario08@gmail.com>
2023-05-15 22:58:00 +0800

    thrid commit

--- searchFlight.php
+++ searchFlight.php
@@ -1,13 +1,15 @@
 <html>
     <head>
-        <title>Aerilon | The world is yours to explore</title>
+    <title>Aerilon | Search Flights</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut icon" type="image-x-icon" href="./resources/favicon.ico">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="./css/style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet">
+    </head>
     <body>
         <ul class="topnav">
             <li class="logo"><img src="./resources/logo.svg" width="10%"></li>
@@ -16,13 +18,30 @@
                 <li class="right"><a href="#">Meet the Crew</a></li>
             </div>
         </ul>
+       
+                <!-- Book now -->
+        <div class="content-container">
+            <div class="content-item" id="content-item">
+                <form>
+                    <div class="content-origin">
+                    <span class="p-flight"><i class="material-icons">flight</i>Flight</span>
+                    <span class="sp-flight-to">To</span>
+                    <input type="text" placeholder="Origin" class="content-textbox-origin">
+                    </div>
+                    <div class="content-destination">
+                    <span class="sp-flight-to">From</span>
+                    <input type="text" placeholder="Destination" class="content-textbox-destination">
+                    </div>
+                    <div class="content-button">
+                    <button type="submit" class="btn-flight">Search flight</button>
+                    </div>
+                </form>
+            </div> 
+        </div>
         <span class="content-text">The world is yours to <span class="content-colored">EXPLORE</span></span>
         <div class="content">
         <div class="sub-content">      
         </div>
         </div>
-        <form action="searchFlight.php" method="post">
-            <button type="submit" class="btn_book" id="btn_book">Book Now</button>
-        </form>
     </body>
 </html>
