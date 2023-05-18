f03b0f88ad8bb416a24ef526e8650ab89f292c9a
Danmar Cornelio P. Nacario <74698488+dcnacario@users.noreply.github.com>
2023-05-18 02:55:56 +0000

    master

---
+++ ticket.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Aerilon | Your Ticket</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" type="image-x-icon" href="./resources/favicon.ico">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="./css/style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <ul class="topnav">
+            <li class="logo"><img src="./resources/logo.svg" width="10%"></li>
+            <div class="right_group">
+                <li class="right_search"><input type="text" placeholder="Search" class="search_bar"></li>
+                <li class="right"><a href="#">Meet the Crew</a></li>
+            </div>
+        </ul>
+        <div class="container_ticket">
+            <h2 class="container_h2">Ticket</h2>
+        </div>
+    </body>
+</html>
