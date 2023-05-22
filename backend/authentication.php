074c3ad31d819243c9888ee5b708352cddaa03c8
Danmar Cornelio Nacario <dcnacario08@gmail.com>
2023-05-23 01:20:38 +0800

    master

--- backend/authentication.php
+++ backend/authentication.php
@@ -1,5 +1,5 @@
 <?php 
-    require('local_setting.php');
+    require('../local_setting.php');
     $username = $_POST['user'];
     $password = $_POST['pass'];
 
@@ -9,7 +9,7 @@
     if (mysqli_num_rows($result) > 0) {
         while($result_row = mysqli_fetch_array($result)){
             if ($result_row['username'] == $username && $result_row['password'] == $password) {
-                header('Location: admin_page.php');
+                header('Location: ../admin_page.php');
             }
         }
     }
